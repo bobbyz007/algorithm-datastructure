f814550be3784a1d304c3f9ba1b0ce47a4085033
justin <pulchritude007@163.com>
2023-04-12 15:27:10 +0800

    MST

--- src/main/java/bz/graph/MST.java
+++ src/main/java/bz/graph/MST.java
@@ -2,8 +2,7 @@ package bz.graph;
 
 import bz.unionfind.UnionFind;
 
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 
 /**
  * <p>MST: 最小生成树 Minimum Spanning Tree
@@ -79,6 +78,9 @@ public class MST {
     /**
      * 适合于稠密图
      * 算法时间复杂度 O(n^2)
+     * 对于给定的连通网，起始状态全部顶点都归为 B 类。在找最小生成树时，选定任意一个顶点作为起始点，并将之从 B 类移至 A 类；
+     * 然后找出 B 类中到 A 类中的顶点之间权值最小的顶点，将之从 B 类移至 A 类，如此重复，直到 B 类中没有顶点为止。
+     * 所走过的顶点和边就是该连通图的最小生成树。
      *
      * @param startVertex 从哪个顶点开始构建最小生成树
      * @param n 顶点个数
@@ -116,12 +118,12 @@ public class MST {
                     minVertex = i;
                 }
             }
-            visit[minVertex] = true; // 标记选择的顶点
+            visit[minVertex] = true; // 标记已找到的顶点
             vertexCnt++;
             minWeightSum += dist[minVertex];
 
             for (int i = 0; i < n; i++) {
-                // 以minVertex可达的点 更新 dist
+                // 以找到的minVertex可达的顶点 更新 dist
                 if (!visit[i] && 0 < adjacentMatrix[minVertex][i] && adjacentMatrix[minVertex][i] < dist[i]) {
                     dist[i] = adjacentMatrix[minVertex][i];
                 }
@@ -137,5 +139,136 @@ public class MST {
             adjacentMatrix[a][b] = adjacentMatrix[b][a] = w;
         }
     }
+
+    // 稀疏图用链式前向星存图，不然存储稠密图需要开更大倍的数组空间
+    final int N = 210;
+    int[] head = new int[N]; // 某个顶点开始的 边集合 的第一条边的索引（边集合以链形式存储）
+    int[] end = new int[N * 2]; // 某条边指向的顶点
+    int[] next = new int[N * 2]; // 某条边的下一条边（有相同起点的边）
+    int[] weight = new int[N * 2]; // 某条边的权重
+    // 基于最小堆优化最短路径计算
+    public int primOptimized(int startVertex, int n, int[][] edges) {
+        buildGraph(edges);
+
+        // 未标记顶点到已生成树的最短距离
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[0] = 0;
+
+        // 以距离排序，堆顶是未标记顶点距离已生成树的最短距离
+        PriorityQueue<long[]> minHeap = new PriorityQueue<>(Comparator.comparingLong(o -> o[1]));
+
+        // 最小权重和
+        int minWeightSum = 0;
+
+        boolean[] visit = new boolean[n];
+        // <顶点编号， 距离>
+        minHeap.offer(new long[]{0, 0});
+        while (!minHeap.isEmpty()) {
+            // 弹出 未标记顶点距离已生成树的最短距离
+            long[] top = minHeap.poll();
+            int vertex = (int) top[0];
+            long distance = top[1];
+            if (visit[vertex]) {
+                continue;
+            }
+            visit[vertex] = true;
+            minWeightSum += distance;
+
+            // 遍历和vertex相邻的点， 更新未标记顶点距离已生成树的最短距离
+            for (int i = head[vertex]; i != -1 ; i = next[i]) {
+                int j = end[i]; // 相邻顶点编号
+                if (!visit[j] && weight[i] < dist[j]) {
+                    dist[j] = weight[i];
+                    minHeap.offer(new long[]{j, dist[j]});
+                }
+            }
+        }
+        return minWeightSum;
+    }
+
+    private void buildGraph(int[][] edges) {
+        // 初始化图
+        Arrays.fill(head, -1);
+        int idx = 0; // 边索引号
+        for (int[] info : edges) {
+            int a = info[0], b = info[1], w = info[2];
+            // 对于无向图，需要存储两条边
+            add(a, b, w, idx++);
+            add(b, a, w, idx++);
+        }
+    }
+
+    private void add(int a, int b, int w, int idx) {
+        end[idx] = b; // 索引idx的边指向顶点b
+        next[idx] = head[a]; // 原来以顶点a开始的 第一条边索引是head[a]，更新next数组
+        head[a] = idx; // 更新head数组，指向当前添加的边。
+
+        weight[idx] = w;
+    }
+
+    // 定义邻接表
+    // private List<int[]>[] adjacentMap = new List[N];
+    private Map<Integer, List<int[]>> adjacentMap = new HashMap<>();
+    // 基于邻接表的最小堆优化最短路径计算
+    public int primOptimizedWithMap(int startVertex, int n, int[][] edges) {
+        buildAdjacentMap(edges);
+
+        // 未标记顶点到已生成树的最短距离
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[0] = 0;
+
+        // 以距离排序，堆顶是未标记顶点距离已生成树的最短距离
+        PriorityQueue<long[]> minHeap = new PriorityQueue<>(Comparator.comparingLong(o -> o[1]));
+
+        // 最小权重和
+        int minWeightSum = 0;
+
+        boolean[] visit = new boolean[n];
+        // <顶点编号， 距离>
+        minHeap.offer(new long[]{0, 0});
+        while (!minHeap.isEmpty()) {
+            // 弹出 未标记顶点距离已生成树的最短距离
+            long[] top = minHeap.poll();
+            int vertex = (int) top[0];
+            long distance = top[1];
+            if (visit[vertex]) {
+                continue;
+            }
+            visit[vertex] = true;
+            minWeightSum += distance;
+
+            // 遍历和vertex相邻的点， 更新未标记顶点距离已生成树的最短距离
+            for (int[] info : adjacentMap.get(vertex)) {
+                int v = info[0], w = info[1];
+                if (!visit[v] && w < dist[v]) {
+                    dist[v] = w;
+                    minHeap.offer(new long[]{v, dist[v]});
+                }
+
+            }
+        }
+        return minWeightSum;
+    }
+
+    private void buildAdjacentMap(int[][] edges) {
+        for (int[] info : edges) {
+            int a = info[0], b = info[1], w = info[2];
+            // 对于无向图，需要存储两条边
+            List<int[]> end = adjacentMap.getOrDefault(a, new ArrayList<>());
+            add(a, b, w);
+            add(b, a, w);
+        }
+    }
+    private void add(int a, int b, int w) {
+        List<int[]> list = adjacentMap.get(a);
+        if (list == null) {
+            list = new ArrayList<>();
+            adjacentMap.put(a, list);
+        }
+        list.add(new int[]{b, w});
+    }
+
 }
 
