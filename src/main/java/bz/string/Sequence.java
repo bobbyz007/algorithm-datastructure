e7db011e14f225defe1e78e27b7b94a8d8e66709
justin <pulchritude007@163.com>
2023-09-05 19:03:23 +0800

    sequence

--- src/main/java/bz/string/Sequence.java
+++ src/main/java/bz/string/Sequence.java
@@ -1,5 +1,6 @@
 package bz.string;
 
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 /**
@@ -33,6 +34,147 @@ public class Sequence {
             }
         }
         return dp[s.length()][t.length()];
+    }
+
+    /**
+     * 最长公共子序列 longest common subsequence
+     */
+    public String lcs(String s, String t) {
+        int[][] dp = new int[s.length() + 1][t.length() + 1];
+        for (int i = 1; i < s.length() + 1; i++) {
+            for (int j = 1; j < t.length() + 1; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        int i = s.length();
+        int j = t.length();
+        StringBuilder sb = new StringBuilder();
+        while (dp[i][j] > 0) {
+            if (dp[i][j] == dp[i - 1][j]) {
+                --i;
+            } else if (dp[i][j] == dp[i][j - 1]) {
+                --j;
+            } else {
+                --i;
+                --j;
+                sb.append(s.charAt(i));
+            }
+        }
+
+        return sb.reverse().toString();
+    }
+
+    /**
+     * 严格升序最长子序列 longest increasing subsequence
+     */
+    public int lis(int[] nums) {
+        if (nums.length <= 1) {
+            return nums.length;
+        }
+
+        // tail 数组的定义：长度为 i + 1 的上升子序列的末尾最小是几
+        int[] tail = new int[nums.length];
+        // 遍历第 1 个数，直接放在有序数组 tail 的开头
+        tail[0] = nums[0];
+        // end 表示有序数组 tail 的最后一个已经赋值元素的索引
+        int end = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            // 【逻辑 1】比 tail 数组实际有效的末尾的那个元素还大
+            if (nums[i] > tail[end]) {
+                // 直接添加在那个元素的后面，所以 end 先加 1
+                end++;
+                tail[end] = nums[i];
+            } else {
+                int left = bisectLeft(tail, end, nums[i]);
+                // 走到这里是left就是第 1 个大于等于 nums[i] 的元素，也就是 tail[left-1] < nums[i] <= tail[left]
+                tail[left] = nums[i];
+            }
+        }
+        // 此时 end 是有序数组 tail 最后一个元素的索引
+        // 题目要求返回的是长度，因此 +1 后返回。 如果要求返回序列，打印tail数组即可。
+        end++;
+        return end;
+    }
+
+    /**
+     * 非降序最长子序列 longest increasing subsequence
+     */
+    public int lisNonDesc(int[] nums) {
+        if (nums.length <= 1) {
+            return nums.length;
+        }
+
+        // tail 数组的定义：长度为 i + 1 的上升子序列的末尾最小是几
+        int[] tail = new int[nums.length];
+        // 遍历第 1 个数，直接放在有序数组 tail 的开头
+        tail[0] = nums[0];
+        // end 表示有序数组 tail 的最后一个已经赋值元素的索引
+        int end = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            // 【逻辑 1】比 tail 数组实际有效的末尾的那个元素还大或相等
+            if (nums[i] >= tail[end]) {
+                // 直接添加在那个元素的后面，所以 end 先加 1
+                end++;
+                tail[end] = nums[i];
+            } else {
+                int left = bisectRight(tail, end, nums[i]);
+                // 走到这里是left就是第 1 个大于等于 nums[i] 的元素，也就是 tail[left-1] < nums[i] <= tail[left]
+                tail[left] = nums[i];
+            }
+        }
+        // 此时 end 是有序数组 tail 最后一个元素的索引
+        // 题目要求返回的是长度，因此 +1 后返回。 如果要求返回序列，打印tail数组即可。
+        end++;
+        return end;
+    }
+
+    /**
+     * tail[left - 1] < x <= tail[left]
+     */
+    private int bisectLeft(int[] tail, int end, int x) {
+        // 使用二分查找法，在有序数组 tail 中 找到第 1 个大于等于 nums[i] 的元素，尝试让tail中那个元素更小
+        int left = 0;
+        int right = end;
+        while (left < right) {
+            // 选左中位数不是偶然，而是有原因的
+            // int mid = left + (right - left) / 2;
+            int mid = left + ((right - left) >>> 1);
+            if (tail[mid] < x) {
+                // 中位数肯定不是要找的数，把它写在分支的前面
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+    /**
+     * tail[left - 1] <= x < tail[left]
+     */
+    private int bisectRight(int[] tail, int end, int x) {
+        // 使用二分查找法，在有序数组 tail 中 找到第 1 个大于等于 nums[i] 的元素，尝试让tail中那个元素更小
+        int left = 0;
+        int right = end;
+        while (left < right) {
+            // 选左中位数不是偶然，而是有原因的
+            // int mid = left + (right - left) / 2;
+            int mid = left + ((right - left) >>> 1);
+            if (x < tail[mid]) {
+                // 中位数肯定不是要找的数，把它写在分支的前面
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
     }
 
 }
--- src/test/java/bz/string/SequenceTest.java
+++ src/test/java/bz/string/SequenceTest.java
@@ -20,4 +20,34 @@ public class SequenceTest {
         Assertions.assertEquals(6, inst.levenshtein("linda", "justin"));
     }
 
+    @Test
+    void testLcs() {
+        Assertions.assertEquals("", inst.lcs("a", "b"));
+        String result = inst.lcs("abcdae", "aedbea");
+        Assertions.assertTrue(result.equals("aba") || result.equals("ade"));
+
+        Assertions.assertEquals("abcd", inst.lcs("jaubsctidnn", "abnnnnnnczzzzzd"));
+    }
+
+    @Test
+    void testLis() {
+
+        Assertions.assertEquals(3, inst.lis(new int[]{3, 5, 6, 1}));
+        Assertions.assertEquals(3, inst.lis(new int[]{3, 5, 6, 1, 2}));
+        Assertions.assertEquals(4, inst.lis(new int[]{3, 5, 6, 1, 2, 7}));
+        Assertions.assertEquals(5, inst.lis(new int[]{3, 5, 6, 1, 2, 7, 8}));
+
+        Assertions.assertEquals(6, inst.lis(new int[]{3, 5, 6, 2, 5, 4, 19, 5, 5, 6, 7, 12}));
+        // 3 5 5 5 5 6 7 12
+        Assertions.assertEquals(8, inst.lisNonDesc(new int[]{3, 5, 6, 2, 5, 4, 19, 5, 5, 6, 7, 12}));
+
+        Assertions.assertEquals(3, inst.lis(new int[]{3, 5, 6}));
+        Assertions.assertEquals(3, inst.lisNonDesc(new int[]{3, 5, 6}));
+
+        Assertions.assertEquals(1, inst.lis(new int[]{3, 3, 3}));
+        Assertions.assertEquals(3, inst.lisNonDesc(new int[]{3, 3, 3}));
+
+        Assertions.assertEquals(2, inst.lis(new int[]{3, 8, 3, 3, 3}));
+        Assertions.assertEquals(4, inst.lisNonDesc(new int[]{3, 8, 3, 3, 3}));
+    }
 }
